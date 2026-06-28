a28aecdf23ec567c014b16bbc3128d3959fa2036
Myduyen <myduyen261104@gmail.com>
2026-06-29 01:24:45 +0700

    fix add card

--- SimpleShop.Repo/Models/Cart.cs
+++ SimpleShop.Repo/Models/Cart.cs
@@ -1,26 +1,38 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SimpleShop.Repo.Models;
 
 [Table("Cart")]
 public class Cart
 {
-    [Key, Column("CartID")] public int CartId { get; set; }
-    [Column("AccountID")] public int AccountId { get; set; }
-    [Column("CreatedDate")] public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    [Key, Column("CartID")]
+    public int CartId { get; set; }
+
+    [Column("AccountID")]
+    public int AccountId { get; set; }
 
     public Account? Account { get; set; }
+
     public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
 }
 
 [Table("CartItem")]
 public class CartItem
 {
-    [Key, Column("CartItemID")] public int CartItemId { get; set; }
-    [Column("CartID")] public int CartId { get; set; }
-    [Column("ProductID")] public int ProductId { get; set; }
-    [Range(1, int.MaxValue), Column("Quantity")] public int Quantity { get; set; }
+    [Key, Column("CartItemID")]
+    public int CartItemId { get; set; }
+
+    [Column("CartID")]
+    public int CartId { get; set; }
+
+    [Column("ProductID")]
+    public int ProductId { get; set; }
+
+    [Range(1, int.MaxValue), Column("Quantity")]
+    public int Quantity { get; set; }
 
     public Cart? Cart { get; set; }
+
     public Product? Product { get; set; }
 }
