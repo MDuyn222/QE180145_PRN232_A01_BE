779130db8be5cfe85e147d135fc166e2f5e47b7e
Myduyen <myduyen261104@gmail.com>
2026-06-30 01:03:59 +0700

    fix cart

--- SimpleShop.API/Controllers/CartController.cs
+++ SimpleShop.API/Controllers/CartController.cs
@@ -1,3 +1,9 @@
+using System.Security.Claims; 
+using Microsoft.AspNetCore.Authorization; 
+using Microsoft.AspNetCore.Mvc; 
+using SimpleShop.Service.DTOs;
+using SimpleShop.Service.Services;
+
 [ApiController]
 [Route("api/cart")]
 [Authorize(Roles = "User")]
--- SimpleShop.Service/obj/Debug/net8.0/SimpleShop.Service.AssemblyInfo.cs
+++ SimpleShop.Service/obj/Debug/net8.0/SimpleShop.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleShop.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4bb2ebca68f2bfcf31a28744b2fc57d0f021c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1092db497faf8377f1565eaa95f0516424166894")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleShop.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleShop.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
